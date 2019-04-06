c62c81a8322593f18f66a5d5da96c064cbbc65fe
kaiwenxiao@outlook.com <kaiwenxiao@outlook.com>
2019-04-06 10:59:38 +0800

    第三章 项目准备

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,34 @@
   </div>
 </template>
 <script>
-  import { mapGetters } from 'vuex'
+  import {mapGetters} from 'vuex'
+
+  const getters = {
+    a: () => 1,
+    b: () => 2
+  }
+
+  function fn(keys) {
+    const data = {}
+    keys.forEach(key => {
+      if (getters.hasOwnProperty(key)) {
+        data[key] = getters[key]
+      }
+    })
+    return data
+  }
 
   export default {
     computed: {
-      ...mapGetters(['test'])
+      ...mapGetters(['test']),
+      ...fn(['a', 'b']) // ...代表可以混入，意思就是数组可以有新增的，会添加到原数组中，例如后面加个 ‘c’，但是都是基于
+      // getters去找的，所以getters也要加上相对应的'C'属性。这样的好处是，直接abc都可以用this表示，（为什么）...实现的吧也许
     },
     mounted() {
-      this.$store.dispatch('setTest', 10).then(() => {
-        console.log(this.test)
-      })
+       this.$store.dispatch('setTest', 10).then(() => {
+         console.log(this.test)
+       })
+      // console.log(this.a, this.b)
     }
   }
   document.addEventListener('DOMContentLoaded', () => {
